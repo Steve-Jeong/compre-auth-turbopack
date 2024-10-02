3b5ec5b63387840da694bbaf6540af06e22826a2
Steve Jeong <jst0930@gmail.com>
2024-10-02 06:12:54 +0300

    daisyui improved

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -6,17 +6,10 @@ const Appbar = () => {
   const toggleDropdown = () => setIsOpen(!isOpen);
   const closeDropdown = () => setIsOpen(false);
   return (
-    <div className="navbar bg-blue-50">
+    <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle"
-            onClick={toggleDropdown}
-            aria-label="Toggle menu"
-            title="Toggle menu"
-          >
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -28,40 +21,60 @@ const Appbar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </div>
-          {isOpen && (
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-blue-50 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li onClick={closeDropdown}>
-                <Link href="/">Homepage</Link>
-              </li>
-              <li onClick={closeDropdown}>
-                <Link href="/features">Features</Link>
-              </li>
-              <li onClick={closeDropdown}>
-                <Link href="/customers">Customers</Link>
-              </li>
-            </ul>
-          )}
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div className="navbar-center">
-        <Link href="/" className="btn btn-ghost text-xl">
-          daisyUI
-        </Link>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
       </div>
-      <div className="navbar-end space-x-2">
-        <Link href="/api/auth/signin" className="btn btn-ghost btn-circle">
-          Login
-        </Link>
-        <Link href="/api/auth/signup" className="btn btn-ghost btn-circle">
-          Signup
-        </Link>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
       </div>
     </div>
   );
